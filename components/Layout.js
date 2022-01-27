a3043f1b422cdb7a67cd862221d31b04ba943ced
lornawanjiru <lornamuchangi@gmail.com>
2022-01-27 16:34:21 +0300

    Mobile view navigation Complete

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,6 @@
 import Footer from './Footer';
 import Nav from './Nav';
 
-  
 
 export default function Layout({children}){
    
--- components/Nav.js
+++ components/Nav.js
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import "@fontsource/fondamento"
 import { Divide as Hamburger } from 'hamburger-react'
 
+
 export default function Nav(){
    
   const [isOpen,setIsOpen] = useState(false);
@@ -18,28 +19,35 @@ export default function Nav(){
               <p>MY PORTFOLIO</p> 
             </div>           
             <div>
+           
             <ul className={isOpen === false ? 
-                styles.navmenu : styles.navmenu +' '+ styles.active }>
+                styles.navmenu +' '+ styles.inactive : styles.navmenu +' '+ styles.active }>
               <li>
+                <br></br>
                 <Link href="/"><a  className ={isOpen === false ? 
                             styles.navlink : styles.navlink+' '+styles.active}
                             onClick={openMenu}>Home</a></Link>
+                            <hr></hr>
               </li>
               <li>
                 <Link href="#"><a  className ={isOpen === false ? 
                             styles.navlink : styles.navlink+' '+styles.active}
                             onClick={openMenu}>Portfolio</a></Link>
+                            <hr></hr>
               </li>
               <li><Link href="#"><a  className ={isOpen === false ? 
                             styles.navlink : styles.navlink+' '+styles.active}
                             onClick={openMenu}>Blogs</a></Link>
+                            <hr></hr>
               </li>
+              <br/>
               <div className={styles.social}>
                <div className={styles.icon}><Image src= "/linkedin.svg" height={25} width={25}/></div>
                <div className={styles.icon}><Image src= "/github.svg" height={25} width={25}/></div>
                <div className={styles.icon}><Image src= "/twitter.svg" height={25} width={25}/></div>
               </div>
             </ul>
+         
             <div className={styles.set}>
             <Hamburger color="purple" easing="ease-in" toggled={isOpen} toggle={setIsOpen} />
             </div>
