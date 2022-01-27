5539fbf62e9118a483676e2280d6d6b42df77cc0
lornawanjiru <lornamuchangi@gmail.com>
2022-01-27 22:17:38 +0300

    desktop view navigation Complete

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,15 @@
 import Footer from './Footer';
 import Nav from './Nav';
-
+import styles from '../styles/Home.module.css'
 
 export default function Layout({children}){
-   
+    
     return(
-    <> 
+    <div className={styles.center}> 
      <Nav /> 
     {children}
     <Footer />
-    </>
+    </div>
  )
      
 }
--- components/Nav.js
+++ components/Nav.js
@@ -2,8 +2,8 @@ import Link from 'next/link'
 import styles from '../styles/Home.module.css';
 import { useState } from "react";
 import Image from 'next/image';
-import "@fontsource/fondamento"
-import { Divide as Hamburger } from 'hamburger-react'
+import "@fontsource/fondamento";
+import { Divide as Hamburger } from 'hamburger-react';
 
 
 export default function Nav(){
@@ -15,10 +15,10 @@ export default function Nav(){
       <header>
         <div className={styles.nav}>
           <nav>
-            <div className={styles.logo}>
-              <p>MY PORTFOLIO</p> 
-            </div>           
-            <div>
+            <div className={styles.hidelogo}>
+              <p>LORNA WANJIRU</p> 
+            </div>        
+            <div className={styles.menu}>
            
             <ul className={isOpen === false ? 
                 styles.navmenu +' '+ styles.inactive : styles.navmenu +' '+ styles.active }>
@@ -41,17 +41,22 @@ export default function Nav(){
                             <hr></hr>
               </li>
               <br/>
+             
               <div className={styles.social}>
                <div className={styles.icon}><Image src= "/linkedin.svg" height={25} width={25}/></div>
                <div className={styles.icon}><Image src= "/github.svg" height={25} width={25}/></div>
                <div className={styles.icon}><Image src= "/twitter.svg" height={25} width={25}/></div>
               </div>
+              
             </ul>
          
             <div className={styles.set}>
-            <Hamburger color="purple" easing="ease-in" toggled={isOpen} toggle={setIsOpen} />
+            <Hamburger color="rgb(208, 208, 208)" easing="ease-in" toggled={isOpen} toggle={setIsOpen} />
             </div>
            </div>
+           <div className={styles.logo}>
+              <p>LORNA WANJIRU</p> 
+            </div>    
           </nav>
          </div>
       </header>
