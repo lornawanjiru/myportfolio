c18869396609c3200f2b703c38be8049fc427e23
lornawanjiru <lornamuchangi@gmail.com>
2022-02-05 03:45:32 +0300

    Classname error

--- components/buttons.js
+++ components/buttons.js
@@ -3,8 +3,8 @@ import styles from '../styles/Home.module.css'
 function Button(){
  return(
     <div className={styles.buttons}>
-        <button class={styles.noselect+' '+ styles.blue}>View Code</button>
-        <button class={styles.noselect+' '+ styles.blue}>Demo Live</button>
+        <button className={styles.noselect+' '+ styles.blue}>View Code</button>
+        <button className={styles.noselect+' '+ styles.blue}>Demo Live</button>
     </div>
  )
 }
