c0bb770e270637f6388eb730359ef904f7c7ece8
lorna wanjiru <lornamuchangi@gmail.com>
2022-03-08 23:02:20 +0300

    head meta

--- pages/index.js
+++ pages/index.js
@@ -12,12 +12,18 @@ export default function Home() {
   return (
     <div className={styles.box}>
       <Head>
-        <title>Lorna Wanjiru Muchangi</title>
-        <meta name="description" content="Lorna Wanjiru Muchangi is an experienced and devoted frontend developer!" />
-        <meta property="og:title" content="Lorna Wanjiru Muchangi" />
+        <title>Lorna Muchangi</title>
+        <meta name="description" content="Lorna Muchangi is an experienced and devoted frontend developer!" />
+        <meta property="og:title" content="Lorna Muchangi" />
         <meta property="og:description" content="Lorna Wanjiru is an experienced and devoted frontend developer!" />
         <meta property="og:url" content="https://www.lornawanjiru.xyz/" />
+        <meta property="og:title" content="Lorna Muchangi"></meta>
+        <meta property="og:locale" content="en-US"></meta>
+        <meta property="og:site_name" content="Lorna Muchangi"></meta>
         <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image"></meta>
+        <meta name="twitter:site" content="@lorna_muchangi"></meta>
+        <meta name="twitter:creator" content="@lorna_muchangi"></meta>
         <link rel="icon" href="/logo1.png" />
       </Head>
       <div className={styles.outerwrapper}>
