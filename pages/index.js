9b6118330d849f9119bd1fb1e9629cc1de443ace
lorna wanjiru <lornamuchangi@gmail.com>
2022-03-08 22:52:57 +0300

    Seo Friendly

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,9 @@ export default function Home() {
     <div className={styles.box}>
       <Head>
         <title>Lorna Wanjiru</title>
-        <meta name="description" content="Portfolio of an experienced and devoted frontend developer!" />
+        <meta name="description" content="Lorna Wanjiru is an experienced and devoted frontend developer!" />
         <meta property="og:title" content="Lorna Wanjiru" />
-        <meta property="og:description" content="Portfolio of an experienced and devoted frontend developer!" />
+        <meta property="og:description" content="Lorna Wanjiru is an experienced and devoted frontend developer!" />
         <meta property="og:url" content="https://www.lornawanjiru.xyz/" />
         <meta property="og:type" content="website" />
         <link rel="icon" href="/logo1.png" />
